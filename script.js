8aa5cf09eaff017a3056d90c5519bad6efc1d387
Daniel silva <danielssilvas123@gmail.com>
2024-06-10 01:20:28 +0000

    Ajuste da função para funcionar em todos os dispositivos e Realmente a FINALIZAÇÃO

--- script.js
+++ script.js
@@ -15,7 +15,8 @@ botoesAssuntos.forEach(botao => {
 })
 
 function selecionarAssunto(evento) {
-    const assunto = evento.target.innerText
+    const classseBotao = evento.target.className
+    const assunto = document.querySelector(`.${classseBotao} span`).innerText
     localStorage.setItem("assunto", assunto)
     window.location.href = "./pages/quiz/quiz.html"
 }
